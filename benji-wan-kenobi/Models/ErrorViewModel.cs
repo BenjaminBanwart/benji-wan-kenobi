ec2337998851fa35362b01a4bba680544cf28059
Benjamin Banwart <BenjiBanwart@Gmail.com>
2023-04-05 17:29:16 -0500

    Add project files.

---
+++ benji-wan-kenobi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace benji_wan_kenobi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
