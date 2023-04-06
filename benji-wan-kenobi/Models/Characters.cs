5e783cbe02870172bb1cdcaf0da9df29cd8ea825
Benjamin Banwart <BenjiBanwart@Gmail.com>
2023-04-05 21:58:13 -0500

    Added bootswatch styling, navbar, and added Characters model and Planets model

---
+++ benji-wan-kenobi/Models/Characters.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace benji_wan_kenobi.Models
+{
+    public class Characters
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Motive { get; set; } = "neutral";
+
+        [DisplayName("Appears In")]
+        public string? AppearsIn { get; set; }
+    }
+}
---
+++ benji-wan-kenobi/Models/Planets.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace benji_wan_kenobi.Models
+{
+    public class Planets
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Capital { get; set; }
+    }
+}
