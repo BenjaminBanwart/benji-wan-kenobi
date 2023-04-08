5ec6018969b0577e787aa19a6bb4c1d13923d3a6
Benjamin Banwart <BenjiBanwart@Gmail.com>
2023-04-08 17:32:45 -0500

    Created planet controller and with create, edit, and view functionality. Created planet views

---
+++ benji-wan-kenobi/Controllers/PlanetController.cs
@@ -0,0 +1,80 @@
+﻿using benji_wan_kenobi.Data;
+using benji_wan_kenobi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace benji_wan_kenobi.Controllers
+{
+    public class PlanetController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PlanetController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Planet> objPlanetList = _db.Planets;
+            return View(objPlanetList);
+        }
+
+        //Get the create planet view
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Route
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Planet objPlanet)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Planets.Add(objPlanet);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(objPlanet);
+        }
+
+        //Get the edit planet view
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var characterFromDb = _db.Planets.Find(id);
+
+            if (characterFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(characterFromDb);
+        }
+
+        //PUT Route
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Planet objPlanet)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Planets.Update(objPlanet);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(objPlanet);
+        }
+    }
+}
