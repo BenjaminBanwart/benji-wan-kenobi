227805872106e7ef9957aa950d26793e998d5294
Benjamin Banwart <BenjiBanwart@Gmail.com>
2023-04-10 19:40:39 -0500

    test deploy

---
+++ benji-wan-kenobi/Migrations/20230411003453_mssql.onprem_migration_816.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace benji_wan_kenobi.Migrations
+{
+    public partial class mssqlonprem_migration_816 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
